800301207aeeb6a4e48c521c6d34b642e815460d
David Andersson <David Andersson>
2018-11-08 10:36:52 +0100

    Console app that consumes the service

---
+++ ConsoleBank/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ConsoleBank.BankServiceReference;
+
+namespace ConsoleBank
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to the bank");
+			using (BankServiceClient proxy = new BankServiceClient())
+			{
+				decimal value = proxy.GetBalance();
+				Console.WriteLine("The account balance is " + value);
+
+				proxy.Deposit(400);
+				value = proxy.GetBalance();
+				Console.WriteLine("We deposited some money, and the new balance is " + value);
+			}
+
+			Console.WriteLine("Press any key to exit");
+			Console.ReadKey();
+		}
+	}
+}
